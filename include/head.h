14bbf403f751c8d8f8a54b0f621ba0ecb28aed5d
lhqhub <lhqgithub@126.com>
2023-04-27 18:58:20 +0800

    2023-04-27 1

---
+++ include/head.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stddef.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <time.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <limits.h>
+#include <float.h>
+#include <errno.h>
+#include <assert.h>
+
